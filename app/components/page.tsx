92161a5ef7fc04ae9491c1e850650339270d3ab3
dandanDQ <47969422+dandanDQ@users.noreply.github.com>
2024-01-14 15:00:30 +0800

    Initial commit

---
+++ app/components/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from './navbar.tsx';
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <div className="header">header</div>
+      <div className="relevant-container">discover</div>
+      <div className="discover-container">discover</div>
+      <div className="navbar">navbar</div>
+    </div>
+  );
+}
