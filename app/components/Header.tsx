fd45062b76bad67a949451bab2170b07c2842d3a
dandanDQ <841294760@qq.com>
2024-02-01 22:15:33 +0800

    fix: remove menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,14 +12,6 @@ export default function Header() {
                             src="/location.svg" alt="" />
                         党演灯塔营地
                     </div>
-                    <div className="menu">
-                        <img
-                            className="icon"
-                            src="/more.svg" alt="" />
-                        <img
-                            className="icon"
-                            src="/back.svg" alt="" />
-                    </div>
                 </div>
                 <div className="slogan">嘟嘟房车          ，创造美好生活！</div>
                 <div className='input-area'>
