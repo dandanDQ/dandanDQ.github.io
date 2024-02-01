5466e52abeb9eb115c2c35e11273cc756f1d1cd4
dandanDQ <841294760@qq.com>
2024-02-01 20:26:11 +0800

    feat: update discoveryCard

--- app/components/DiscoveryCard.tsx
+++ app/components/DiscoveryCard.tsx
@@ -6,11 +6,21 @@ export default function DiscoveryCard(
 ) {
   return (
     <div className="discovery-card">
+      <div className="card-img-controller-wrapper">
+        <img
+          className="icon"
+          src='/like_black.svg'
+        />
+      </div>
       <img
         className="img"
         src={img}
         alt=""
       />
+      <div className="discovery-mask">
+        <div className="main-info">航空版C型</div>
+        <div className="description">￥1999</div>
+      </div>
     </div>
   );
 }
